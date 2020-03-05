c76b4d6addbd1f4b242ccee418e3e612d1606c09
Robert Demourelle <robert.demourelle@gmail.com>
2020-03-04 21:32:51 -0600

    fixed routing names

--- src/routes.js
+++ src/routes.js
@@ -6,21 +6,21 @@ import TableList from "./views/TableList.jsx";
 const dashboardRoutes = [
   {
     path: "/dashboard",
-    name: "Dashboard",
+    name: "dashboard",
     icon: "pe-7s-graph",
     component: Dashboard,
     layout: "/admin"
   },
   {
     path: "/upload",
-    name: "Upload File",
+    name: "upload",
     icon: "pe-7s-cloud-upload",
     component: Upload,
     layout: "/admin"
   },
   {
     path: "/table",
-    name: "Uploaded Animations",
+    name: "table",
     icon: "pe-7s-note2",
     component: TableList,
     layout: "/admin"
